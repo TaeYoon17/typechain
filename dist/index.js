5eda79a6bd188ae5f6df7bb779753ffa1e787011
root <root@LAPTOP-9EI67U4D.localdomain>
2021-01-10 01:09:36 +0900

    #interface

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const person = {
+    name: "kim",
+    gender: "male",
+    age: 22
+};
 const name = "Kim", age = 23, gender = "male";
-const sayHi = (name, age, gender) => {
+const sayHi = (person) => {
     return `Hello ${name}, you are ${age}, you are a ${gender}!`;
 };
-console.log(sayHi(name, age, gender));
+console.log(sayHi(person));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
+interface Human {
+    name: string;
+    age: number;
+    gender: string;
+}
+
+const person={
+    name:"kim",
+    gender:"male",
+    age:22
+};
+
 const name = "Kim",
     age=23,
     gender="male";
 
-const sayHi = (name:string,age:number,gender:string):string=>{
+const sayHi = (person:Human):string=>{
     return `Hello ${name}, you are ${age}, you are a ${gender}!`;
 };
 
-console.log(sayHi(name,age,gender));
+console.log(sayHi(person));
 
 export {};
